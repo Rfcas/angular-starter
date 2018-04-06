899b5e02f56fa0a42da0d8f9eaefa8b5bd5384b5
Rafael Castro <rafael.castro@thought-creator.com>
2018-04-06 16:55:12 +0100

    Todo step3

--- src/state/state.module.ts
+++ src/state/state.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools'; 
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { counterReducer } from '@state/counter/counter.reducer';
+import { todoListReducer } from '@state/todo-list/reducer';   // add
 
 @NgModule({
     imports: [
         StoreModule.forRoot({
-            count: counterReducer
+            count: counterReducer,
+            todoList: todoListReducer                                   // add
         }),
         StoreDevtoolsModule.instrument()
     ]
